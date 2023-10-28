3fbb00517cade676c8901c0b0a23455902c47b2c
Sebastian Gonzalez <seba.gnh@gmail.com>
2023-10-28 00:11:40 -0300

    se agregaron los números del 3600

--- VerNumeros.bat
+++ VerNumeros.bat
@@ -1,12 +1,12 @@
 @echo off
 rem Unknown
-set "NumeroCPU=99999999999"
+set "NumeroCPU=Unknown"
 rem System Serial Number
-set "NumeroBIOS=8888888888"
+set "NumeroBIOS=System Serial Number"
 rem 200569744400562
-set "NumeroMotherboard=Default string"
+set "NumeroMotherboard=200569744400562"
 rem E3F9F6C8-1299-E04B-D990-D45D64B80BB8
-set "NumeroUUID=03C00218-044D-056F-4206-C90700080009"
+set "NumeroUUID=E3F9F6C8-1299-E04B-D990-D45D64B80BB8"
 rem Mac
 rem D4-5D-64-B8-0B-B8   \Device\Tcpip_{EDC80A0E-C33E-41CE-89CF-A7498F15C84A}
 title VerNumeracos
@@ -15,56 +15,61 @@ color 0F
 echo **********************************
 :start
 cls
-wmic diskdrive get model, serialnumber
 echo -----------------------------
-echo CPU
-rem CPU
+rem CPU---------------------------
 for /f "tokens=2 delims==" %%I in ('wmic cpu get serialnumber /value') do set "cpu_serial=%%I"
 
 if "%cpu_serial%"=="%NumeroCPU%" (
+    echo CPU  PELIGRO
     echo %NumeroCPU% ORIGINAL  PELIGRO
     echo %cpu_serial% FRESCO    PELIGRO
 ) else (
+    echo CPU  SEGURO
     echo %NumeroCPU% ORIGINAL  SEGURO
     echo %cpu_serial% FRESCO    SEGURO
 )
 echo -----------------------------
-echo BIOS
-rem BIOS
+rem BIOS--------------------------
 for /f "tokens=2 delims==" %%I in ('wmic bios get serialnumber /value') do set "bios_serial=%%I"
 
 if "%bios_serial%"=="%NumeroBIOS%" (
+    echo BIOS  PELIGRO
     echo %NumeroBIOS% ORIGINAL  PELIGRO
     echo %bios_serial% FRESCO    PELIGRO
 ) else (
+    echo BIOS  SEGURO
     echo %NumeroBIOS% ORIGINAL  SEGURO
     echo %bios_serial% FRESCO    SEGURO
 )
 echo -----------------------------
-echo Motherboard
-rem Motherboard
+rem Motherboard------------------
 for /f "tokens=2 delims==" %%I in ('wmic baseboard get serialnumber /value') do set "board_serial=%%I"
 
 if "%board_serial%"=="%NumeroMotherboard%" (
+    echo Motherboard  PELIGRO
     echo %NumeroMotherboard% ORIGINAL  PELIGRO
     echo %board_serial% FRESCO    PELIGRO
 ) else (
+    echo Motherboard  SEGURO
     echo %NumeroMotherboard% ORIGINAL  SEGURO
     echo %board_serial% FRESCO    SEGURO
 )
 echo -----------------------------
-echo smBIOS UUID
-rem smBIOS UUID
+rem smBIOS UUID-------------------
 for /f "tokens=2 delims==" %%I in ('wmic path win32_computersystemproduct get uuid /value') do set "uuid=%%I"
 
 if "%uuid%"=="%NumeroUUID%" (
-    echo Numero anterior PELIGRO
+    echo UUID  PELIGRO
     echo %NumeroUUID% ORIGINAL  PELIGRO
     echo %uuid% FRESCO    PELIGRO
 ) else (
+    echo UUID  SEGURO
     echo %NumeroUUID% ORIGINAL  SEGURO
     echo %uuid% FRESCO    SEGURO
 )
+echo -----------------------------
+echo Discos
+wmic diskdrive get model, serialnumber
 echo -----------------------------
 getmac
 echo Press any key to get your hardware serials again.
